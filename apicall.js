5476851aff12ad5ccd0155491b1e7546a12e5e99
Zander404 <alexandrearthurpg@gmail.com>
2022-12-09 21:42:21 -0300

    minor changes

--- apicall.js
+++ apicall.js
@@ -2,10 +2,17 @@
 
 // Constantes se objetivo ainda
 let id = 1; 
-const type = 'pokemon';
+
 let estoque = 100;
 
+//Pokemon info 
+let pokemonImage = ''
+let pokemonName = ''
+let pokemonlist = []
 
+
+
+//Paginação
 let next_page
 let prev_page
 let list = 'https://pokeapi.co/api/v2/pokemon'
@@ -23,18 +30,10 @@ const tHeadInfo = document.getElementById('info_header_table');
 
 
 //Botões 
-const saibaMais = document.querySelector(".btn-info")
+const saibaMais = document.getElementsByClassName("btn-info")
 const prev = document.querySelector(".btn-prev")
 const next = document.querySelector(".btn-next")
 
-
-
-
-const getPokemon = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
-    const data = response.json();
-    return data;
-}
 
 
 const fetchApi = async(list) => {
@@ -47,81 +46,89 @@ const fetchApi = async(list) => {
 }
 
 
-
-const getInfo = async (url) => {
-    const data = await fetchApi(url)
-
-    
-    for(let j=0; j<saibaMais.length;j++){
-        saibaMais[j].addEventListener("click", ()=>{
-        infoP = data.results[j].url
-        estoque=estoque-1;
-        console.log(estoque)
-
-        info = getInfo(infoP).then(data =>{
-
-            console.log(data)
-            if (data){
-            tdBodyTableInfo.innerHTML = ''
-            const info = `
-                <tr>
-                <td class="bg-black text-white border border-white border-radius rounded-md ">${data.name}</td>
-                <td class="bg-black text-white border border-white border-radius rounded-md ">${data.abilities[0].ability.name} & ${data.abilities[1].ability.name}</td>
-                
-                
-                </tr>
-            `
-            tdBodyTableInfo.innerHTML += info
-            }
-            
-        })
-        })
-    }
-    
-    
-}
-
-
-// // Povoar tabela com os Pokémons disponivéis
-
-
-
+// Povoar tabela com os Pokémons disponivéis
 const renderList = async (lista) =>{
     const data = await fetchApi(lista)
     if(data.results){
-        console.log(data)
+        // console.log(data)
         next_page = data.next
         prev_page = data.previous
     
         
-    tdBodyTable.innerHTML = ''
+        tdBodyTable.innerHTML = ''
 
-    let ps = id
-    data.results.forEach((element) => {
-        const pokemon = `
-        <tr>
-        <th class="bg-black text-white border border-white border-radius rounded-md ">${ps}</th>
-        <th class="bg-black text-white border border-white border-radius rounded-md ">${element.name}</th>
-        <th><button type="submit" class="btn btn-info  bg-white border-radius rounded-md border border-black btn-info">"Saber mais"</button></th>
-        
-        </tr>
-        `
-        ps += 1;
-        tdBodyTable.innerHTML += pokemon
+        let ps = id
 
-        
-    });
 
+        data.results.forEach((element) => {
+            const pokemon = `
+            <tr>
+            <th class="bg-black text-white border border-white border-radius rounded-md ">${ps}</th>
+            <th class="bg-black text-white border border-white border-radius rounded-md ">${element.name}</th>
+            <th><button type="submit" class="btn btn-info  bg-white border-radius rounded-md border border-black">"Saber mais"</button></th>
+            
+            </tr>
+            `
+            ps += 1;
+            tdBodyTable.innerHTML += pokemon
+
+
+            //Paginação das Infos do Pokémon
+            for(let j=0; j<saibaMais.length;j++){
+                saibaMais[j].addEventListener("click", ()=>{
+                    infoP = data.results[j].url
+                    estoque=estoque-1;
+                    // console.log(estoque)
+                    info = fetchApi(infoP).then(data =>{
+                        // console.log(data)
+                        let flag = 0
+
+                        if (data){
+                            for(i=0;i<pokemonlist.length;i++){
+                                if (pokemonlist[i].id == data['id']){
+                                    flag = 1
+                                }
+
+                            }
+
+                            if (flag != 1){
+                                pokemonlist.push({id: data['id'], quant:10})
+                                console.log(pokemonlist)
+                                pokemonName = data['name']
+                                
+                                pokemonImage =  data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+                                const info = `
+                                    <tr>
+                                        <td class="bg-black text-white border border-white border-radius rounded-md ">${pokemonName}</td>
+                                        <td class="bg-black text-white border border-white border-radius rounded-md "><img src="${pokemonImage}" alt="pokemon-img" class="pokemon_image"></td>
+                                    </tr>
+                            `
+                            tdBodyTableInfo.innerHTML += info
+                            }else{
+                                quantPokemon = pokemonlist[{id:'data.id'}].quant
+                                console.log('teste')
+                                pokemonlist[data['id']].quant = quantPokemon-1
+                                flag = 0 
+                            }
+                        }
+                    })
+                })
+            }  
+            
+        });
+    }else{
+        alert("Erro")
     }
         
 }
 
 
+//Rederizar a primeira pagina 
 renderList(list)
 
 
 //Paginação 
-
 prev.addEventListener("click", () => {
     event.preventDefault()
     if (prev_page != 'null'){
@@ -145,12 +152,5 @@ next.addEventListener("click", () => {
 
 
 
-saibaMais.addEventListener('click', () => {
-    event.preventDefault()
-    if(pinfo != 'null'){
-        alert("Clicko")
-    }else{
-
-    }
-})
+// Info dos Pokemons
 
